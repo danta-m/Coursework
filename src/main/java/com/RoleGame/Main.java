e96c6df9a151444acd1171adc94588da81738f27
Bogdana Maksiuta <danetqt@gmail.com>
2021-09-26 20:18:26 +0300

    Add 5 users to DB

--- src/main/java/com/RoleGame/Main.java
+++ src/main/java/com/RoleGame/Main.java
@@ -6,6 +6,10 @@ import com.RoleGame.database.Init;
 public class Main {
     public static Init initDB = new Init();
 
+    public static IncidentDAO incidentDAO = new IncidentDAOImpl();
+    public static ProfileDAO profileDAO = new ProfileDAOImpl();
+    public static ServiceDAO serviceDAO = new ServiceDAOImpl();
+    public static UserDAO userDAO = new UserDAOImpl();
 
     public static void main(String[] args) {
 
